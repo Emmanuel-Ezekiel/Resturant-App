8680196ebea915eb943af627293f12df76f470dc
Emmanuel-EEzekiel <emamex44@gmail.com>
2021-11-29 05:32:29 +0100

    add file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
+import {
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Restaurant from './components/restaurant';
+import RestaurantList from './components/restaurant-list';
+import AddReview from './components/add-review';
+import Login from './components/login';
+
+
 
 function App() {
+  const [user, setUser] = React.useState(null);
+
+  async function login(user = null) {
+    setUser(user);
+  }
+
+  async function logout() {
+    setUser(null)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div>
+       <nav className="navbar navbar-expand navbar-dark bg-dark">
+        <a href="/restaurant" className="navbar-brand">
+          Restaurant Reviews
         </a>
-      </header>
+        <div className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <Link to="/restaurant" className="nav-link">
+              Restaurants
+            </Link>
+          </li>
+          <li className="nav-item" >
+            { user ? (
+              <span  onClick={logout} className="nav-link" style={{cursor:'pointer'}}>
+                Logout {user.name}
+              </span>
+            ) : (            
+            <Link to="/login" className="nav-link">
+              Login
+            </Link>
+            )}
+
+          </li>
+        </div>
+      </nav>
+    <div className="container mt-3">
+        <Routes>
+            {["/restaurant", "/"].map((path, index) => 
+        <Route exact path={path} element={<RestaurantList/>} key={index} />
+    )}
+          <Route 
+            path="/restaurant/:id/review"
+            render={(props) => (
+              <AddReview {...props} user={user} />
+            )}
+          />
+          <Route 
+            path="/restaurant/:id"
+            render={(props) => (
+              <Restaurant {...props} user={user} />
+            )}
+          />
+          <Route 
+            path="/login"
+            render={props =>  < Login {...props} login={login} /> }
+          />
+        </Routes>
+      
+      </div>
     </div>
   );
 }
